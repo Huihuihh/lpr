43ffd1e57d67bad953c9a7e1107ecc03dac7d4bb
Huihuihh <tianhui@awcloud.com>
2020-12-10 17:22:51 +0800

    完善训练

--- lpr/run_test.py
+++ lpr/run_test.py
@@ -19,8 +19,6 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-from lpr.trainer import decode_beams
-
 
 r_vocab={0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '<Anhui>', 11: '<Beijing>', 12: '<Chongqing>', 13: '<Fujian>', 14: '<Gansu>', 15: '<Guangdong>', 16: '<Guangxi>', 17: '<Guizhou>', 18: '<Hainan>', 19: '<Hebei>', 20: '<Heilongjiang>', 21: '<Henan>', 22: '<HongKong>', 23: '<Hubei>', 24: '<Hunan>', 25: '<InnerMongolia>', 26: '<Jiangsu>', 27: '<Jiangxi>', 28: '<Jilin>', 29: '<Liaoning>', 30: '<Macau>', 31: '<Ningxia>', 32: '<Qinghai>', 33: '<Shaanxi>', 34: '<Shandong>', 35: '<Shanghai>', 36: '<Shanxi>', 37: '<Sichuan>', 38: '<Tianjin>', 39: '<Tibet>', 40: '<Xinjiang>', 41: '<Yunnan>', 42: '<Zhejiang>', 43: '<police>', 44: 'A', 45: 'B', 46: 'C', 47: 'D', 48: 'E', 49: 'F', 50: 'G', 51: 'H', 52: 'I', 53: 'J', 54: 'K', 55: 'L', 56: 'M', 57: 'N', 58: 'O', 59: 'P', 60: 'Q', 61: 'R', 62: 'S', 63: 'T', 64: 'U', 65: 'V', 66: 'W', 67: 'X', 68: 'Y', 69: 'Z', 70: '_', -1: ''}
 
@@ -31,6 +29,15 @@ def load_graph(frozen_graph_filename):
   with tf.Graph().as_default() as graph:
     tf.import_graph_def(graph_def)
   return graph
+
+def decode_beams(vals, r_vocab):
+  beams_list = []
+  for val in vals:
+    decoded_number = ''
+    for code in val:
+      decoded_number += r_vocab[code]
+    beams_list.append(decoded_number)
+  return beams_list
 
 def display_license_plate(number, license_plate_img):
   size = cv2.getTextSize(number, cv2.FONT_HERSHEY_SIMPLEX, 0.55, 2)
